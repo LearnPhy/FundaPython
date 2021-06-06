7938b893682624ad32d93d9bf2b76a0663d1690a
Luthfi M Ilham <luthfim.ilham@gmail.com>
2021-06-07 06:24:49 +0700

    perulangan array dengan for

--- perulangan.py
+++ perulangan.py
@@ -1,3 +1,10 @@
 cars = 4
 for car in range(1, cars + 1):
     print(f'the car #{car}')
+
+# array
+kucing = ['eko', 'dwi', 'trio', 'catur']
+
+print(kucing)
+for kc in kucing:
+    print(f'hi {kc}')
