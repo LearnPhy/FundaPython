f09488f40b700cfb9aca753bad8d9d8110f0108c
Luthfi M Ilham <luthfim.ilham@gmail.com>
2021-06-09 06:32:31 +0700

    function

---
+++ fungsi/segitiga.py
@@ -0,0 +1,6 @@
+def calculate_triangle_area(base, height):
+    triangle_area = base * height / 2
+    return triangle_area
+
+
+print(f'luas segitiga dengan panjang alas 10cm dan tinggi 4cm adalah {calculate_triangle_area(10,4)}cm2')
---
+++ geometry/triangle.py
@@ -0,0 +1,3 @@
+def calculate_triangle_area(base, height):
+    triangle_area = base * height / 2
+    return triangle_area
